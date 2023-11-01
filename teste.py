dff4ccfc58003f970311c9bf56d2aeadc26b16f0
Gustavo <112349322+GustavoZen@users.noreply.github.com>
2023-11-01 13:42:28 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -4,6 +4,9 @@ from pokemon_list import pokemons_primeira_geracao
 from pokemon_list import tipos_pokemon_primeira_geracao
 import os
 
+class MyButton (tk.Button):
+    tipos = ""
+
 def on_canvas_configure(event):
     canvas.configure(scrollregion=canvas.bbox("all"))
 
@@ -29,14 +32,19 @@ def BarraDeRolagem(janela):
 
     return scrollbar, frame_elementos, canvas, frame_mestre
 
+def botao1(janela):
+    print("Tamanho atual da janela: " + str(janela.winfo_width()) + " x " + str(janela.winfo_height()))
+    
 # Criando a janela principal
 janela = tk.Tk()
 janela.title("Minha Página Tkinter")
-janela.geometry("800x450")
+janela.geometry("652x505")
+janela.resizable(width=False, height=False)
 
 scrollbar, frame_elementos, canvas, frame_mestre = BarraDeRolagem(janela)
 cont = 0
 linha = 1
+listaDeBotoes = []
 while(cont<151):
     for coluna in range(5):
         if cont < 151:
@@ -45,15 +53,17 @@ while(cont<151):
             imagem = tk.PhotoImage(file=caminho_imagem)
             
             texto_var = tk.StringVar()
-            texto_var.set(pokemons_primeira_geracao[cont] + " #" + str(cont+1) + "\n" +tipos_pokemon_primeira_geracao[cont])
+            texto_var.set(pokemons_primeira_geracao[cont] + " #" + str(cont+1) + "\n" + tipos_pokemon_primeira_geracao[cont])
             
             # Criando o botão com a imagem e o texto
-            botao = tk.Button(frame_mestre, image=imagem, textvariable=texto_var, compound='top',height=100,width=100)
+            botao = MyButton(frame_mestre, image=imagem, textvariable=texto_var, compound='top',height=100,width=100)
             botao.imagem = imagem  # Para evitar que a imagem seja coletada pelo garbage collector
             botao.grid(row=linha, column=coluna, padx=10, pady=10)
+            listaDeBotoes.append(botao)
             
             cont += 1
     linha += 1
 
 # Iniciando o loop principal
+listaDeBotoes[1]['command'] = lambda:botao1(janela)
 janela.mainloop()
